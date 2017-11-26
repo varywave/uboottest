e7af3608dc96cf61e141c1eb76b090df3c560bad
varywave <info@varywave.com>
2017-11-26 16:43:13 +0800

    delete compiled bins

--- am335x/include/config.h
+++
@@ -1,9 +0,0 @@
-/* Automatically generated - do not edit */
-#define CONFIG_EMMC_BOOT	1
-#define CONFIG_BOARDDIR board/ti/am335x
-#include <config_defaults.h>
-#include <config_uncmd_spl.h>
-#include <configs/am335x_evm.h>
-#include <asm/config.h>
-#include <linux/kconfig.h>
-#include <config_fallbacks.h>
